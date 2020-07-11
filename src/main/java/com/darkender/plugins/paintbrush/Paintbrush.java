d9792013fff0106d8581b9516c8445c6dc53c27f
Techdoodle <26680599+TheTechdoodle@users.noreply.github.com>
2020-07-11 08:47:19 -0700

    add color cycle detection

--- src/main/java/com/darkender/plugins/paintbrush/Paintbrush.java
+++ src/main/java/com/darkender/plugins/paintbrush/Paintbrush.java
@@ -11,20 +11,65 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Paintbrush extends JavaPlugin implements Listener
 {
     private static NamespacedKey paintbrushKey;
+    private static List<String> colors;
+    private static Map<String, List<Material>> cycles;
     
     @Override
     public void onEnable()
     {
         paintbrushKey = new NamespacedKey(this, "paintbrush");
+        colors = Arrays.asList("white", "orange", "magenta", "light_blue", "yellow",
+                "lime", "pink", "gray", "light_gray", "cyan", "purple", "blue", "brown",
+                "green", "red", "black");
+        cycles = new HashMap<>();
+        for(Material material : Material.values())
+        {
+            String key = material.getKey().getKey();
+            for(String color : colors)
+            {
+                if(key.startsWith(color + "_"))
+                {
+                    String name = key.substring(color.length() + 1);
+                    if(!cycles.containsKey(name))
+                    {
+                        cycles.put(name, new ArrayList<>());
+                    }
+                    cycles.get(name).add(material);
+                }
+            }
+        }
+        
+        Iterator<Map.Entry<String, List<Material>>> entryIterator = cycles.entrySet().iterator();
+        while(entryIterator.hasNext())
+        {
+            Map.Entry<String, List<Material>> entry = entryIterator.next();
+            if(entry.getValue().size() == 1)
+            {
+                entryIterator.remove();
+                continue;
+            }
+            
+            entry.getValue().sort((first, second) ->
+            {
+                String firstKey = first.getKey().getKey();
+                String colorFirst = firstKey.substring(0, firstKey.length() - entry.getKey().length() - 1);
+                String secondKey = second.getKey().getKey();
+                String colorSecond = secondKey.substring(0, secondKey.length() - entry.getKey().length() - 1);
+                
+                return colors.indexOf(colorFirst) - colors.indexOf(colorSecond);
+            });
+        }
         
         PaintbrushCommand paintbrushCommand = new PaintbrushCommand();
         getCommand("paintbrush").setExecutor(paintbrushCommand);
         getCommand("paintbrush").setTabCompleter(paintbrushCommand);
+        
+        getServer().getPluginManager().registerEvents(this, this);
     }
     
     public static ItemStack generatePaintbrush()
